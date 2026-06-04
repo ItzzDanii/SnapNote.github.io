b5ced3ac3e681bfd857a01608f9e46d64f45ed08
ItzzDanii <barbuto.daniel29@hotmail.com>
2026-06-04 20:44:21 +0200

    Update theme stylesheet paths in script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // Codice per la pagina home
 function temaDefault() {
     let tema = document.getElementById("tema");
-    tema.href = "./home_style2.css";
+    tema.href = "/SnapNote/home_style2.css";
 }
 
 function cambiaTema() {
     let tema = document.getElementById("tema");
 
-    if (tema.href.includes("./home_style1.css")) {
-        tema.href = "./home_style2.css";
+    if (tema.href.includes("/SnapNote/home_style1.css")) {
+        tema.href = "/SnapNote/home_style2.css";
     } else {
-        tema.href = "./home_style1.css";
+        tema.href = "/SnapNote/home_style1.css";
     }
 }
 
